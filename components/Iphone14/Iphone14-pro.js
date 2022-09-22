c99f16a8d31d7cdbf2419cf2e87226415fb98192
mbukeRepo <mbuke404@gmail.com>
2022-09-22 06:04:04 +0200

    updated landing page

--- components/Iphone14/Iphone14-pro.js
+++ components/Iphone14/Iphone14-pro.js
@@ -5,18 +5,16 @@ import CTAButtons from '../UI/CTAButton/CTAButtons';
 export default function Iphone14() {
   return (
     <section className='iphone14-pro'>
-      <div className='iphone14-pro__container'>
-        <div className='iphone14-pro__header'>
-          <h2 className='iphone14-pro__title'>iPhone 14 Pro</h2>
-          <p className='iphone14-pro__subtitle'>Pro. Beyond.</p>
-          <CTAButtons />
-        </div>
-        <img
-          src='https://www.apple.com/v/home/ar/images/heroes/iphone-14-pro/hero_iphone14pro__e5xbgo5ffhg2_mediumtall.jpg'
-          alt=''
-          className='iphone14-pro__image'
-        />
+      <div className='iphone14-pro__header'>
+        <h2 className='iphone14-pro__title'>iPhone 14 Pro</h2>
+        <p className='iphone14-pro__subtitle'>Pro. Beyond.</p>
+        <CTAButtons />
       </div>
+      <img
+        src='https://www.apple.com/v/home/ar/images/heroes/iphone-14-pro/hero_iphone14pro__e5xbgo5ffhg2_mediumtall.jpg'
+        alt=''
+        className='iphone14-pro__image'
+      />
     </section>
   );
 }
--- components/Iphone14/Iphone14.js
+++ components/Iphone14/Iphone14.js
@@ -5,21 +5,19 @@ import CTAButtons from '../UI/CTAButton/CTAButtons';
 export default function Iphone14() {
   return (
     <section className='iphone-14'>
-      <div className='iphone-14__container'>
-        <div className='iphone-14__header'>
-          <h2 className='iphone-14__title'>iPhone 14</h2>
-          <h4 className='iphone-14__subtitle'>Big and bigger.</h4>
-          <p className='iphone-14__desc'>
-            iPhone&nbsp;14&nbsp;Plus available starting 10.7
-          </p>
-          <CTAButtons />
-        </div>
-        <img
-          src='https://www.apple.com/v/home/ar/images/heroes/iphone-14/hero_iphone14_preorder__c4g2wjzuugqe_medium.jpg'
-          alt=''
-          className='iphone14__image'
-        />
+      <div className='iphone-14__header'>
+        <h2 className='iphone-14__title'>iPhone 14</h2>
+        <h4 className='iphone-14__subtitle'>Big and bigger.</h4>
+        <p className='iphone-14__desc'>
+          iPhone&nbsp;14&nbsp;Plus available starting 10.7
+        </p>
+        <CTAButtons />
       </div>
+      <img
+        src='https://www.apple.com/v/home/ar/images/heroes/iphone-14/hero_iphone14_preorder__c4g2wjzuugqe_medium.jpg'
+        alt=''
+        className='iphone14__image'
+      />
     </section>
   );
 }
